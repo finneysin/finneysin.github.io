2c266b395650f0c10f0bd81c977bd1174d316fb4
Dallas Finney <finneydallas1337@gmail.com>
2016-08-29 23:23:31 +0000

    Made /button/

---
+++ button/main.js
@@ -0,0 +1,10 @@
+var oniichan
+var oniichan = new Audio ("oniichan.mp3")
+oniichan.play();
+
+function sayoniichan() {
+    var oniichan = new Audio("oniichan.mp3")
+    oniichan.play();
+}
+
+$("#onii").on("click", sayoniichan);
