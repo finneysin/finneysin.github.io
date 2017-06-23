9a1efb73d4b8fba8df1bbfe8011e335da92fed05
Dallas Finney <finneydallas1337@gmail.com>
2017-06-23 03:53:04 +0000

    lol

--- rave/main.js
+++
@@ -1,8 +0,0 @@
-var thisHoldsThePaths = ["music/comehome.mp3", "music/lights.mp3","music/pumpedupkicks.mp3"];
-
-var indexNumber = Math.floor(Math.random() * (thisHoldsThePaths.length));
-
-var audio = new Audio(thisHoldsThePaths[indexNumber]);
-audio.load();
-audio.play();
-
--- weeb/main.js
+++
@@ -1,8 +0,0 @@
-var thisHoldsThePaths = ["music/badapple.mp3", "music/heartbreak.mp3","music/caramell.mp3"];
-
-var indexNumber = Math.floor(Math.random() * (thisHoldsThePaths.length));
-
-var audio = new Audio(thisHoldsThePaths[indexNumber]);
-audio.load();
-audio.play();
-
